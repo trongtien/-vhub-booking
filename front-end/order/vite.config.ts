07c5ad4f4ebf7db971d03dcf764012899db8de99
tienplt <phanletrongtien@gmail.com>
2026-01-21 14:07:55 +0700

    Refactor code structure for improved readability and maintainability

--- front-end/order/vite.config.ts
+++ front-end/order/vite.config.ts
@@ -1,27 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'order',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './App': './src/App.tsx',
-      },
-      shared: {
-        react: {
-          requiredVersion: '^19.2.0',
-        },
-        'react-dom': {
-          requiredVersion: '^19.2.0',
-        },
-        'react-router-dom': {},
-      },
-    }),
-  ],
+  plugins: [react()],
   build: {
     modulePreload: false,
     target: 'esnext',
@@ -32,17 +13,10 @@ export default defineConfig({
     port: 3002,
     strictPort: true,
     cors: true,
-    origin: 'http://localhost:3000',
     headers: {
       'Access-Control-Allow-Origin': '*',
     },
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost',
-      port: 3002,
-    }
   },
-  base: "/order/",
   preview: {
     port: 3002,
     strictPort: true,
